9dcce823dc2cf31bd41649aa48d5bfddff7bbb56
Sheelam Aravind <102232179+AravindSheelam@users.noreply.github.com>
2023-08-22 21:01:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,71 +1,61 @@
-// Get the required elements
-const video = document.querySelector('video');
-const progressBar = document.querySelector('.progress__filled');
-const playButton = document.querySelector('.player__button');
-const volumeInput = document.querySelector('input[name="volume"]');
-const playbackSpeedInput = document.querySelector('input[name="playbackRate"]');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress__filled');
+const toggleButton = document.querySelector('.player__button');
+const volumeSlider = document.querySelector('input[name="volume"]');
+const playbackSpeedSlider = document.querySelector('input[name="playbackRate"]');
 const skipButtons = document.querySelectorAll('[data-skip]');
-const speedBar = document.querySelector('.speed-bar');
 
-// Add event listeners for play/pause button
-playButton.addEventListener('click', togglePlay);
-
-// Add event listener for progress update
-video.addEventListener('timeupdate', updateProgress);
-
-// Add event listener for progress bar click
-progressBar.parentElement.addEventListener('click', scrub);
-
-// Add event listeners for volume and playback speed
-volumeInput.addEventListener('input', handleVolumeChange);
-playbackSpeedInput.addEventListener('input', handlePlaybackSpeedChange);
-
-// Add event listeners for skip buttons
-skipButtons.forEach(button => button.addEventListener('click', skip));
-
-// Update play/pause button
-function updatePlayButton() {
-  playButton.textContent = video.paused ? '►' : '❚ ❚';
-}
-
-// Toggle play/pause on button click
 function togglePlay() {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
-  updatePlayButton();
+	if (video.paused) {
+		video.play();
+	}else{
+		video.pause();
+	}
 }
 
-// Update progress bar
-function updateProgress() {
-  const percent = (video.currentTime / video.duration) * 100;
-  progressBar.style.flexBasis = `${percent}%`;
+function updateToggleButton() {
+	const icon = video.paused ? '►' : '❚ ❚';
+	toggleButton.innerContent = icon;
 }
 
-// Scrub through the video
+function handleVolumeChange() {
+	video.volume = volumeSlider.value;
+}
+
+function handlePlaybackSpeedChange() {
+	video.playbackRate = playbackSpeedSlider.value;
+}
+
+function handleSkip() {
+	const skipSeconds = parseFloat(this.dataset.skip);
+	video.currentTime += skipSeconds;
+}
+
+function handleProgress() {
+	const progressPercentage = (video.currentTime / video.duration) * 100;
+    progress.style.flexBasis = `${progressPercentage}%`;
+}
+
 function scrub(e) {
-  const scrubTime = (e.offsetX / progressBar.parentElement.offsetWidth) * video.duration;
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
   video.currentTime = scrubTime;
 }
 
-// Handle volume change
-function handleVolumeChange() {
-  video.volume = this.value;
-}
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateToggleButton);
+video.addEventListener('pause', updateToggleButton);
+video.addEventListener('timeUpdate', handleProgress);
 
-// Handle playback speed change
-function handlePlaybackSpeedChange() {
-  video.playbackRate = this.value;
-  speedBar.textContent = `${this.value}×`;
-}
+toggleButton.addEventListener('click', togglePlay);
 
-// Skip forward or backward
-function skip() {
-  const skipTime = parseFloat(this.dataset.skip);
-  video.currentTime += skipTime;
-}
+volumeSlider.addEventListener('input', handleVolumeChange);
 
-// Initial setup
-updatePlayButton();
+playbackSpeedSlider.addEventListener('input', handlePlaybackSpeedChange);
+
+skipButtons.forEach(button => button.addEventListener('click', handleSkip));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
